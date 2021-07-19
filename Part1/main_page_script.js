7f91dc371cab128ada191517430afb4dac02226c
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-07-19 03:11:04 -0700

    Bonus part of the first part added.

--- Part1/main_page_script.js
+++ Part1/main_page_script.js
@@ -20,5 +20,78 @@ function slides_show(){
         }
 
     }
-    setTimeout(slides_show, 5000);
+    setTimeout(slides_show, 10000);
 }
+
+function slider_image_back() {
+
+    if(image_counter == 0){
+        document.getElementById("clock_image").src = "Images/clock2.png";
+        image_counter = 2;
+
+    }
+    else {
+        if(image_counter == 1){
+            document.getElementById("clock_image").src = "Images/clock0.png";
+            image_counter = 0;
+
+        }
+        else {
+            if(image_counter == 2){
+                document.getElementById("clock_image").src = "Images/clock1.png";
+                image_counter = 1;
+
+            }
+
+        }
+
+    }
+
+}
+
+function slider_image_next() {
+
+    if(image_counter == 0){
+        document.getElementById("clock_image").src = "Images/clock1.png";
+        image_counter = 1;
+
+    }
+    else {
+        if(image_counter == 1){
+            document.getElementById("clock_image").src = "Images/clock2.png";
+            image_counter = 2;
+
+        }
+        else {
+            if(image_counter == 2){
+                document.getElementById("clock_image").src = "Images/clock0.png";
+                image_counter = 0;
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
